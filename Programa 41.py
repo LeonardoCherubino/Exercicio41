b3d4d639303d859b965b7b853dcda94bc09a8039
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2022-03-16 08:18:47 -0300

    Meu primeiro IF

---
+++ Programa 41.py
@@ -0,0 +1,9 @@
+#Programa 4.1 - Lê dois números e imprime qual é o maior
+
+a = int(input('Entre com um valor: '))
+b = int(input('Entre com mais um valor: '))
+if a > b:
+    print('O primeiro valor é maior!')
+if b > a:
+    print('O segundo valor é maior')
+    
